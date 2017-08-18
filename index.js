7bc30f8e8ef2efd21c3d0bab5366e9678872eae5
Kolosowski florent <fkolosowski@ivsweb.com>
2017-08-18 15:30:50 +0200

    WIP : now use puppeteer instead of phantomjs ! (RIP phantom 2.5)

--- index.js
+++ index.js
@@ -1,161 +1,54 @@
 "use strict";
 
-const fs      = require('fs');
-const bind    = require('bindthem');
-const system  = require('system');
+const fs        = require('fs');
+const path      = require('path');
+const bind      = require('bindthem');
+const puppeteer = require('puppeteer');
 
-const sleep      = require('nyks/function/sleep');
-const defer      = require('nyks/promise/defer');
+const rmrf       = require('nyks/fs/rmrf');
 const sprintf    = require('nyks/string/format');
 const startsWith = require('mout/string/startsWith');
 const ltrim      = require('mout/string/ltrim');
 const forOwn     = require('mout/object/forOwn');
+const merge      = require('mout/object/merge');
 
 const DEFAULT_OPTIONS = {
-  coverage : false
+  coverage        : false,
+  width           : 1024,
+  height          : 768,
+  screenshots_dir : './screenshots',
+  screenshots_ext : '.jpg',
+  address         : 'http://127.0.0.1:8000' // default express url
 };
 
 module.exports = class GhostRider {
-  constructor(page, address, options) {
-    this.address = address;
+
+  constructor(options) {
+
+    var current_node_version = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
+
+    if (current_node_version < 7)
+      throw "Your node version must be at least 7 !";
+
+    this.options = merge(DEFAULT_OPTIONS, options);
 
     this.driver = null;
+    this.page   = null;
 
     this.is_main = (this.constructor === GhostRider.prototype.constructor);
-
-    this.options = DEFAULT_OPTIONS;
-    this.parse_args();
-
-    this.screenshots_basedir   = './';
-    this.screenshots_extension = '.jpg';
-
-    this.screenshots_dir = '';
 
     this.available_actions = [
       'click', 'screenshot', 'waitFor', 'wait', 'play'
     ];
 
     bind(this, this.available_actions);
-
-    this.page = page;
-    this.page.onConsoleMessage = function(msg, lineNum, sourceId) {
-      console.log(msg);
-    };
-
   }
 
-  parse_args() {
-    system.args.forEach(arg => {
-      if (startsWith(arg, '-')) {
-        let option = ltrim(arg, '-').split('=');
-        this.options[option[0]] = option[1] || true;
-      }
-    });
-  }
-
-  get current_screenshots_dir() {
-    return sprintf('%s/%s', this.screenshots_basedir, this.screenshots_dir);
-  }
-
-  page_open() {
-    var defered = defer();
-    this.page.open(this.address, defered.resolve);
-    return defered;
-  }
-
-  * waitFor(args) {
-    var selector = args.selector || 'body';
-    var parentFrame = args.parentFrame || false;
-    var checkvisible = args.untilVisible || false;
-
-    var found = this.page.evaluate(this.foundElement, selector, parentFrame, checkvisible);
-    if(found) {
-      console.log('Found ' + selector);
-      return Promise.resolve();
-    }
-    yield sleep(300);
-    yield this.waitFor(args);
-  }
-
-  foundElement(selector, parentFrame, checkvisible) {
-    
-    var isVisible = function(element) {
-
-      if (element.offsetWidth === 0 || element.offsetHeight === 0) return false;
-
-      var height = root.documentElement.clientHeight;
-      var rects = element.getClientRects();
-      var on_top = function(r) {
-        var x = (r.left + r.right)/2, y = (r.top + r.bottom)/2;
-        return element.contains( root.elementFromPoint(x, y) );
-      };
-
-      for (var i = 0, l = rects.length; i < l; i++) {
-        var r = rects[i];
-        var in_viewport = r.top > 0 ? r.top <= height : (r.bottom > 0 && r.bottom <= height);
-        if (in_viewport && on_top(r)) return true;
-      }
-      return false;
-    }
-
-    var root = document;
-    if(parentFrame) {
-      var iframe = document.querySelector(parentFrame);
-      if(!iframe)
-        return;
-      root = iframe.contentWindow.document;
-    }
-
-    var found = root.querySelector(selector);
-    if(!found)
-      return false;
-
-    if(checkvisible)
-      return isVisible(found);
-
-    return true;
-  }
-
-  * click(selector){
-     this.page.evaluate(function(selector) {
-      document.querySelector(selector).click();
-    }, selector);
-    console.log('clicked on ' + selector);
-    return Promise.resolve();
-  }
-
-  * screenshot(screenshot_name) {
-    if (!fs.isDirectory(this.current_screenshots_dir))
-      fs.makeDirectory(this.current_screenshots_dir);
-
-
-    let screenshot_path = sprintf("%s/%s%s", this.current_screenshots_dir, screenshot_name, this.screenshots_extension);
-    console.log(sprintf("Take a screenshot, save it in %s", screenshot_path))
-    this.page.render(screenshot_path);
-    return Promise.resolve();
-  }
-
-  * play(script) {
-    this.page.evaluate(eval("(function() { " + script + " })"));
-    return Promise.resolve();
-  }
-
-  * wait(time) {
-    yield sleep(time);
-    return Promise.resolve();
-  }
-
-  * ride(scenario) {
+  async ride(scenario) {
 
     this.driver = scenario;
     if (!this.driver)
-      return Promise.reject();
-
-    if (this.driver.settings.screenshots_basedir)
-      this.screenshots_basedir = this.driver.settings.screenshots_basedir;
-
-    if (this.driver.settings.screenshots_extension)
-      this.screenshots_extension = this.driver.settings.screenshots_extension;
+      throw "No scenario sended";
 
     if (this.driver.alias) {
       forOwn(this.driver.alias, (alias, name) => {
@@ -166,28 +59,95 @@ module.exports = class GhostRider {
       });
     }
 
-    if (!fs.isDirectory(this.screenshots_basedir))
-      fs.makeDirectory(this.screenshots_basedir);
+    let screenshots_path = path.resolve(process.cwd(), this.options.screenshots_dir);
 
-    yield this.readScenario(this.driver.scenario.slice(0));
+    await rmrf(screenshots_path);
+
+    if (!fs.existsSync(screenshots_path))
+      fs.mkdirSync(screenshots_path);
+
+    var browser = await this.page_open();
+
+    await this.readScenario(this.driver.scenario.slice(0));
 
     if (this.options.coverage)
-      this._write_coverage();
+      await this.write_coverage();
 
-    return Promise.resolve();
+    browser.close();
   }
 
-  _write_coverage() {
-    if (!this.is_main)
-      return; // Ghost rider has been extended, do not write coverage automatically
+  async page_open() {
+    var browser = await puppeteer.launch();
+    this.page   = await browser.newPage();
 
-    this.write_coverage();
-  }
-
-  write_coverage() {
-    var coverage = this.page.evaluate(function() {
-      return window.__coverage__;
+    this.page.setViewport({
+      width  : this.options.width,
+      height : this.options.height
     });
+
+    await this.page.goto(this.options.address);
+
+    return browser;
+  }
+
+
+  async readScenario(scenario) {
+
+    if (!scenario.length)
+      return;
+
+    var currentTask = scenario[0];
+    scenario.shift();
+
+    var method = (typeof currentTask == "string") ? currentTask : Object.keys(currentTask)[0];
+
+    if (typeof this[method] == 'function')
+      await this[method](currentTask[method]);
+
+    if (typeof this[method] == 'object')
+      await this.readScenario(this[method].slice(0));
+
+    await this.readScenario(scenario);
+  }
+
+  async waitFor(args) {
+    var selector  = args.selector       || 'body';
+    var visible   = args.untilVisible   || false;
+    var invisible = args.untilInvisible || false;
+
+    console.log('waitfor', {selector, visible, invisible});
+
+    if (invisible) {
+      let frame = this.page.mainFrame();
+      await frame.waitForFunction("$('" + selector + "').length == 0");
+      return;
+    }
+
+    await this.page.waitForSelector(selector, {visible});
+  }
+
+  async screenshot(screenshot_name) {
+    let screenshot_file = sprintf('%s%s', screenshot_name, this.options.screenshots_ext);
+    let screenshot_path = path.resolve(this.current_screenshots_dir, screenshot_file);
+    console.log(sprintf("Take a screenshot in %s", screenshot_path));
+    await this.page.screenshot({path: screenshot_path});
+  }
+
+  async play(script) {
+    await this.page.evaluate(eval("(function() { " + script + " })"));
+  }
+
+  async click(selector) {
+    await this.page.click(selector);
+    console.log('clicked on ' + selector);
+  }
+
+  get current_screenshots_dir() {
+    return path.resolve(process.cwd(), this.options.screenshots_dir);
+  }
+
+  async write_coverage() {
+    var coverage = await this.page.evaluate("window.__coverage__");
 
     if (coverage) {
       console.log('Writing coverage to coverage/coverage.json');
@@ -197,23 +157,15 @@ module.exports = class GhostRider {
     }
   }
 
-  * readScenario(scenario) {
-
-    if (!scenario.length)
-      return Promise.resolve();
-
-    var currentTask = scenario[0];
-    scenario.shift();
-
-    var method = (typeof currentTask == "string") ? currentTask : Object.keys(currentTask)[0];
-
-    if (typeof this[method] == 'function')
-      yield this[method](currentTask[method]);
-
-    if (typeof this[method] == 'object')
-      yield this.readScenario(this[method].slice(0));
-
-    yield this.readScenario(scenario);
+  /*
+  parse_args() {
+    process.argv.forEach(arg => {
+      if (startsWith(arg, '-')) {
+        let option = ltrim(arg, '-').split('=');
+        this.options[option[0]] = option[1] || true;
+      }
+    });
   }
+  */
 
 }
